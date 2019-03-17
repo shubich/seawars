f03109a5a740af2edca75c72bc4bbe99e4be77ca
shubich <shaa5@yahoo.com>
2019-03-17 16:56:21 +0300

    Add moves to game

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -4,50 +4,44 @@ import seaReducer from 'reducers/sea/seaReducer';
 import { getInitialSeaState } from 'reducers/sea/helpers';
 import { fireToCoordinates, AIFireToCoordinates } from 'actions/sea/seaActions';
 import AIShot from 'utils/AI/AIShot';
+import { ICoordinates } from 'types/seaTypes';
 
 import './Game.scss';
 
+const { useEffect, useReducer, useCallback } = React;
+
 const Game: React.FC = () => {
-  const [state, dispatch] = React.useReducer(seaReducer, getInitialSeaState());
+  const [state, dispatch] = useReducer(seaReducer, getInitialSeaState());
 
-  // const aiMove = React.useCallback(() => {
-  //   dispatch(
-  //     AIFireToCoordinates(AIShot(state.playerSea, state.playerShipInProgress)),
-  //   );
-  // }, [state]);
+  const aiMove = useCallback(() => {
+    dispatch(
+      AIFireToCoordinates(AIShot(state.playerSea, state.playerShipInProgress)),
+    );
+  }, [state]);
 
-  // React.useEffect(() => {
-  //   const intervalId = setInterval(aiMove, 500);
+  useEffect(() => {
+    if (!state.isPlayerTurn) {
+      setTimeout(aiMove, 1000);
+    }
+  }, [state]);
 
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, [state]);
+  const playerMove = useCallback(
+    (coordinates: ICoordinates) => {
+      if (state.isPlayerTurn) {
+        dispatch(fireToCoordinates(coordinates));
+      }
+    },
+    [state],
+  );
 
   return (
     <div className="Game">
       <div className="Game-Manage">
         <h1>{state.isPlayerTurn ? 'Player' : 'AI'} turn</h1>
-        <button
-          // tslint:disable-next-line: jsx-no-multiline-js
-          onClick={() => {
-            dispatch(
-              AIFireToCoordinates(
-                AIShot(state.playerSea, state.playerShipInProgress),
-              ),
-            );
-          }}
-        >
-          AI attack
-        </button>
       </div>
       <div className="Game-Sea">
         <SeaChart sea={state.playerSea} />
-        <SeaChart
-          isEnemy={true}
-          sea={state.enemySea}
-          fire={coordinates => dispatch(fireToCoordinates(coordinates))}
-        />
+        <SeaChart isEnemy={true} sea={state.enemySea} fire={playerMove} />
       </div>
     </div>
   );
